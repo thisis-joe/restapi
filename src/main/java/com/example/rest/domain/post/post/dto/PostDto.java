3703f2e80b2f715e735bc05fae9a94e4db16a8f2
joseph <wdg0434@gmail.com>
2025-02-07 03:44:35 +0900

    feat#14 : Apply @JsonIgnore to change column name anytime

--- src/main/java/com/example/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/example/rest/domain/post/post/dto/PostDto.java
@@ -1,19 +1,30 @@
 package com.example.rest.domain.post.post.dto;
 import com.example.rest.domain.post.post.entity.Post;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
+    @JsonIgnore
     private LocalDateTime createdDate;
+    @JsonIgnore
     private LocalDateTime modifiedDate;
     private String title;
     private String content;
+    
     public PostDto(Post post) {
         this.id = post.getId();
         this.createdDate = post.getCreatedDate();
         this.modifiedDate = post.getModifiedDate();
         this.title = post.getTitle();
         this.content = post.getContent();
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdDate;
+    }
+    public LocalDateTime getModifiedAt() {
+        return modifiedDate;
     }
 }
