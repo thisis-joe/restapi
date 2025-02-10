ffa354dba07476479bc3504a97a41daf2f8c81e7
joseph <wdg0434@gmail.com>
2025-02-10 10:46:14 +0900

    feat#24: 204 no content

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -53,11 +53,14 @@ public class ApiV1PostController {
                         ) {}
 
     @PutMapping("{id}")
-    public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
+    public ResponseEntity modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
         Post post = postService.getItem(id).get();
         postService.modify(post, body.title(), body.title());
 
-        return new RsData<>("200-2", "%d번 글 수정이 완료되었습니다.".formatted(id),null);
+        return ResponseEntity
+                .noContent()
+                .build();
+        //return new RsData<>("200-2", "%d번 글 수정이 완료되었습니다.".formatted(id),null);
     }
 
     record WriteReqBody(@NotBlank @Length(min=3)String title,
