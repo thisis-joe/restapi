ca51ec2d91d9d621cae7f2e028478895169572eb
joseph <wdg0434@gmail.com>
2025-02-10 12:09:54 +0900

    feat#27: Testing instance

--- src/test/java/com/example/rest/RestApplicationTests.java
+++ src/test/java/com/example/rest/RestApplicationTests.java
@@ -23,5 +23,22 @@ class RestApplicationTests {
 			int a = list.get(i);
 		}
 	}
+	@Test
+	void t2() {
+		ArrayList list = new ArrayList();
+		list.add(1);
+		list.add("2");
+		list.add(3);
+		if(list.get(1) instanceof String) {
+			System.out.println("1번째 값은 문자열입니다.");
+		}
+		for(int i = 0; i < list.size(); i++) {
+			if( list.get(i) instanceof Integer) {
+				int a = (int)list.get(i);
+			} else if(list.get(i) instanceof String) {
+				String a = (String)list.get(i);
+			}
+		}
+	}
 
 }
