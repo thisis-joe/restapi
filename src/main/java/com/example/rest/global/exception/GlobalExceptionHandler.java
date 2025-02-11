cb6cb2a97b9e7570e21037510b8614ee71cd47de
joseph <wdg0434@gmail.com>
2025-02-11 09:34:28 +0900

    feat#34: Add Valid fail exception on GlobalExceptionHandler

--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -2,12 +2,13 @@ package com.example.rest.global.exception;
 import com.example.rest.global.dto.RsData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler(NoSuchElementException.class) //없는 데이터 요청 시
     public ResponseEntity<RsData<Void>> handle() {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
@@ -17,5 +18,9 @@ public class GlobalExceptionHandler {
                                 "해당 데이터가 존재하지 않습니다"
                         )
                 );
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class) //입력값 검증 실패
+    public String handleMethodArgumentNotValidException() {
+        return "MethodArgumentNotValidException";
     }
 }
